6e7047909727688d795d8b44fff2fc94ebb3ce01
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-23 15:44:09 -0600

    merged my portfolio code with Jason's lecture code for Heroku

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-import App from './App'
-import Home from './Home'
-import Blog from './Blog'
-import Projects from './Projects'
+import Main from './main'
 
 render(
   (
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <Route path="/home" component={ Home } />
-        <Route path="/blog" component={ Blog } />
-        <Route path="/projects" component={ Projects } />
-      </Route>
-    </Router>
+   <Main/>
   ),
   document.getElementById('app')
 )

9219bd63abfd4a27bed8eb56612f339572ba09ca
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-24 14:04:46 -0600

    Uppercased my Projects page and added a return and section that was left blank

---
+++ js/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default React.createClass({
+  render() {
+    return(
+      <section>
+        <h1> Shaurice's Resume </h1>
+      </section>
+    )
+  }
+})
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './app'
 import Main from './main'
-import Projects from './projects'
+import Projects from './Projects'
 import Blog from './Blog'
 import Resume from './Resume'
 
@@ -13,7 +13,7 @@ render(
     <Router history={hashHistory}>
       <Route component={App}>
         <Route path="/home" component={Main} />
-        <Route path="/projects" component={ Projects } />
+        <Route path="/Projects" component={ Projects } />
         <Route path="/Blog" component={ Blog } />
         <Route path="/Resume" component={ Resume } />
       </Route>

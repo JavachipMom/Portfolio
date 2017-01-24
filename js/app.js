589618323f33391d83c91f391b7f2b2c2595cd00
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-24 12:09:18 -0600

    Fixed my esseential problems.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router'
 import Header from './Header'
 import Footer from './Footer'
-import Main from './Main'
 
 export default React.createClass({
   render() {
--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './app'
 import Main from './main'
+import Projects from './projects'
+
 
 render(
   (
-   <Main/>
+    <Router history={hashHistory}>
+      <Route component={App}>
+        <Route path="/" component={Main} />
+        <Route path="/project/:name/:url" component={Project} />
+      </Route>
+    </Router>
   ),
   document.getElementById('app')
 )
